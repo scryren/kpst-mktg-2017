1900ebee73cda2222a3f723269abe8d764392989
scryren <scryren@gmail.com>
2017-09-29 11:57:23 -0600

    Updated Express Optin Language

--- source/assets/js/jquery.privacy-statement-kpst.js
+++ source/assets/js/jquery.privacy-statement-kpst.js
@@ -15,7 +15,7 @@
 $(document).ready(function () {
 	//$('#privacy-statement-checkbox').css('display','show');
 
-	$('#privacy-statement').append('<div id="privacy-statement-checkbox"><input id="legalCheckbox" name="legalCheckbox" type="checkbox" value="on" data-validation="required" data-validation-depends-on="Marketing_Country__c" data-validation-depends-on-value="Canada, Germany, Australia" data-validation-error-msg="You must agree to Kapost&rsquo;s Terms of Service." data-validation-error-msg-container="#email-error-dialog"><span class="privacy-statement">By submitting this form, I acknowledge and agree to the storing and processing of my personal data by Kapost as described in their <a href="https://kapost.com/privacy-policy/" target="_blank">Privacy Policy</a>, as well as Kapost&rsquo;s use of my contact information to communicate with me regarding Kapost&rsquo;s products, services, and events.</span>');
+	$('#privacy-statement').append('<div id="privacy-statement-checkbox"><input id="legalCheckbox" name="legalCheckbox" type="checkbox" value="on" data-validation="required" data-validation-depends-on="Marketing_Country__c" data-validation-depends-on-value="Canada, Germany, Australia" data-validation-error-msg="You must agree to Kapost&rsquo;s Terms of Service." data-validation-error-msg-container="#email-error-dialog"><span class="privacy-statement">By submitting this form, I acknowledge and agree to the storing and processing of my personal data by Kapost as described in their <a href="https://kapost.com/privacy-policy/" target="_blank">Privacy Policy</a>, as well as Kapost&rsquo;s use of my contact information to communicate with me regarding Kapost&rsquo;s products, services, and events. I may unsubscribe at any time.</span>');
 
 });
 
