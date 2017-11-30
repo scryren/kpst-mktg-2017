6ff366b0ab94af6e700b6a1f104fda7c1ed28175
scryren <scryren@gmail.com>
2017-11-29 17:58:14 -0700

    Float Labels

---
+++ source/assets/js/jquery-float-label.js
@@ -0,0 +1,51 @@
+/**********************************
+Title: Float Label Form Interaction
+Author: Captain Anonymous 
+URL: https://codepen.io/anon/pen/GHKJj
+**********************************/
+
+$(function ()
+{
+  var onClass = "on";
+  var showClass = "show";
+
+  $("input, select")
+    .bind("checkval", function ()
+    {
+      var label = $(this).prev("label");
+
+      if (this.value !== "")
+        label.addClass(showClass);
+
+      else
+        label.removeClass(showClass);
+    })
+    .on("keyup", function ()
+    {
+      $(this).trigger("checkval");
+    })
+    .on("focus", function ()
+    {
+      $(this).prev("label").addClass(onClass);
+    })
+    .on("blur", function ()
+    {
+        $(this).prev("label").removeClass(onClass);
+    })
+    .trigger("checkval");
+
+  $("select")
+    .on("change", function ()
+    {
+      var $this = $(this);
+
+      if ($this.val() == "")
+        $this.addClass("watermark");
+
+      else
+        $this.removeClass("watermark");
+
+      $this.trigger("checkval");
+    })
+    .change();
+});
